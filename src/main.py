c49cf62dea1dd7a649363adb89c893a0ac371132
Praso28 <pranay.solanki2811@gmail.com>
2025-03-22 05:12:32 +0530

    Updated solution

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import sys
+import time
 from collections import defaultdict
 from math import ceil
 
 def process_file():
+    start_time = time.time() 
+
     city_data = defaultdict(lambda: [float("inf"), 0, float("-inf"), 0]) 
     data = sys.stdin.read().splitlines()
 
@@ -17,10 +20,16 @@ def process_file():
         stats[2] = max(stats[2], score)   
         stats[3] += 1
                          
-    sys.stdout.write("\n".join(
-        f"{city}={data[0]:.1f}/{ceil((data[1] / data[3]) * 0.1 * 10) / 10:.1f}/{data[2]:.1f}"
-        for city, data in sorted(city_data.items())
-    ) + "\n")
+    output_file_path = "/usr/src/app/output/result.txt" 
+    with open(output_file_path, "w") as f:
+        f.write("\n".join(
+            f"{city}={data[0]:.1f}/{ceil((data[1] / data[3]) * 0.1 * 10) / 10:.1f}/{data[2]:.1f}"
+            for city, data in sorted(city_data.items())
+        ) + "\n")
 
+    end_time = time.time() 
+    elapsed_time = round((end_time - start_time) * 1000, 2)  
+
+    print(f"Processing time: {elapsed_time}ms") 
 if __name__ == "__main__":
     process_file()
